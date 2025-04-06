df1eec7cfe1746243a488c67352c5a4ecff81e1c
oskaldev <artem-osipovartem3002@mail.ru>
2025-04-06 16:39:03 +0300

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<script>
+  import HomePage from './Pages/Todo/HomePage.vue'
+  export default {
+    components: {
+      HomePage
+    },
+  }
+</script>
+
+<template>
+  <div class="p-5">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <HomePage />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/Todo/HomePage.vue
@@ -0,0 +1,18 @@
+<script>
+  import AppListTodo from '@/Components/Todo/AppListTodo.vue'
+
+  export default {
+    components: {
+      AppListTodo
+    },
+  }
+</script>
+
+<template>
+  <div>
+    Home Page !
+    <AppListTodo />
+  </div>
+</template>
+
+<style scoped></style>
