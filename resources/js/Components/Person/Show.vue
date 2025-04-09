eecf726fc6c81e30570b22a99c04c802fc2c850e
oskaldev <artem-osipovartem3002@mail.ru>
2025-04-10 01:21:27 +0300

    update

--- resources/js/Components/Person/Show.vue
+++ resources/js/Components/Person/Show.vue
@@ -8,7 +8,6 @@
       }
     },
     mounted() {
-      console.log(this.$route.params)
       this.getPerson()
     },
     methods: {
@@ -24,19 +23,19 @@
 </script>
 
 <template>
-  <div class="container mx-auto my-10">
+  <div v-if="person" class="container mx-auto my-10">
     <h3 class="text-center text-3xl font-semibold mb-4">Person</h3>
     <div class="md:w-1/2 mx-auto">
       <div class="bg-white shadow-md rounded-lg p-6">
         <div class="flex flex-wrap justify-center mb-4">
-          <div v-if="person" class="w-full px-4 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">Name: {{ person.name }}</div>
-          <div v-if="person" class="w-full px-4 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">Age: {{ person.age }}</div>
-          <div v-if="person" class="w-full px-4 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">Job: {{ person.job }}</div>
+          <div class="w-full px-4 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">Name: {{ person.name }}</div>
+          <div class="w-full px-4 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">Age: {{ person.age }}</div>
+          <div class="w-full px-4 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500">Job: {{ person.job }}</div>
           <div>
             <router-link :to="{ name: 'home' }" class="cursor-pointer m-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
               Home page
             </router-link>
-            <router-link v-if="person" :to="{ name: 'person.edit', params: { id: person.id } }" class="cursor-pointer m-3 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+            <router-link :to="{ name: 'person.edit', params: { id: person.id } }" class="cursor-pointer m-3 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
               Edit
             </router-link>
           </div>
